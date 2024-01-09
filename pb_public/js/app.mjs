93f7cb5d330cd18118b40c40348bc7fbeb2e4647
Claudemiro Alves Feitosa Neto <dimiro1@gmail.com>
2024-01-09 21:23:06 +0100

    chore: Hello World with mithril

---
+++ pb_public/js/app.mjs
@@ -0,0 +1,5 @@
+import {Hello} from "./hello.mjs";
+
+const root = document.querySelector("#root");
+
+m.mount(root, Hello);
---
+++ pb_public/js/hello.mjs
@@ -0,0 +1,3 @@
+export const Hello = {
+    view: () => m("h1", "Hello World")
+};
